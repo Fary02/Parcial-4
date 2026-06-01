d5c6fb9dd303c705552004c6b4505cc052716d15
Fary02 <267945234+Fary02@users.noreply.github.com>
2026-06-01 16:28:40 -0400

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,18 @@
+# Arrays 
+
+notas = [5.5, 2.7, 5.0]; 
+
+edad = int(input("\nDime tu edad: ")); 
+
+nombre = str(input("\nDime tu nombre: ")); 
+
+carrera = str(input("\nDime tu carrera: ")); 
+
+estudiante = {
+    
+    "nombre": nombre,
+    "edad": edad,
+    "carrera": carrera
+}; 
+
+print(f"Sus datos: {estudiante}"); 

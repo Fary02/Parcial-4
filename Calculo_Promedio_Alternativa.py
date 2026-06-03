f103a20c92911e31ea87fb24cf2bb33cb8dafb3c
Fary02 <267945234+Fary02@users.noreply.github.com>
2026-06-03 09:47:25 -0400

    Add files via upload

---
+++ Calculo_Promedio_Alternativa.py
@@ -0,0 +1,11 @@
+# Se genera un array;
+
+notas = [3.5, 7.0, 5.6]; 
+
+# Se calcula el promedio; 
+
+promedio = sum(notas) / len(notas); 
+
+# Se imprime el promedio;
+
+print(promedio.__round__(1)); 
